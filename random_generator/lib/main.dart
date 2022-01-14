4dc2fe51ee181697a6c131ff1762024ef37c5caa
Arora-Yash-coder <yasharora9416@gmail.com>
2022-01-14 13:23:47 +0530

    Random Word Gen

--- random_generator/lib/main.dart
+++ random_generator/lib/main.dart
@@ -23,11 +23,46 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Random Word Generator'),
       ),
-      body: Center(
-        child: Text(
-          wordPair.asPascalCase,
-          style: TextStyle(fontSize: 40),
-        ),
+      body: Center(child: NewWidget()),
+    );
+  }
+}
+
+class NewWidget extends StatefulWidget {
+  @override
+  _NewWidgetState createState() => _NewWidgetState();
+}
+
+class _NewWidgetState extends State<NewWidget> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
