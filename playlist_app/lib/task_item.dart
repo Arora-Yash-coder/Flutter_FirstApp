a6b573fdd9b122d7ee7626822132f043e3ea2fc3
Yash Arora <yasharora9416@gmail.com>
2022-04-03 16:44:34 +0530

    Chore: Creating a  new repo for the deleted proj

---
+++ playlist_app/lib/task_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TaskItem extends StatefulWidget {
+  final String label;
+
+  TaskItem({Key? key, required this.label}) : super(key: key);
+
+  @override
+  _TaskItemState createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  bool? _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          onChanged: (newValue) => setState(() => _value = newValue),
+          value: _value,
+        ),
+        Text(widget.label),
+      ],
+    );
+  }
+}
